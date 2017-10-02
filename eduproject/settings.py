5fb7767bc185caed48c3e4706e95f83f89b882a6
thiernomoudou <souleymanemoudou@gmail.com>
2017-10-02 13:11:39 +0000

    add postgresql config and update requirements.txt

--- eduproject/settings.py
+++ eduproject/settings.py
@@ -88,10 +88,21 @@ WSGI_APPLICATION = 'eduproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': '',
+        'PORT': '',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
 
 # Password validation
